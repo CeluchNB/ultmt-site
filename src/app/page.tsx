4759db6b1767b7d3aac8eab4a51db55757f2695c
Noah Celuch <noah.celuch@gmail.com>
2023-08-02 21:16:27 -0400

    feat: update store links to actual store listing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div style={{ textAlign: 'center' }}>
       <Navigation />
       <main className="flex min-h-screen flex-col items-center p-8">
-        <a href="https://www.apple.com/app-store/">
+        <a href="https://apps.apple.com/us/app/the-ultmt-app/id6452383295">
           <Image
             src="./app-store.svg"
             alt="Download on the App Store"
@@ -14,7 +14,7 @@ export default function Home() {
             height={96}
           />
         </a>
-        <a href='http://play.google.com/store/'>
+        <a href="https://play.google.com/store/apps/details?id=com.ultmtapp">
           <img
             alt="Get it on Google Play"
             src="./play-store.png"
