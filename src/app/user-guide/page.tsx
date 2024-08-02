bca884f491ceec45c7b9e655b918f6842d0dda81
Noah Celuch <noah.celuch@gmail.com>
2024-08-01 22:14:23 -0400

    wip(guide): add more sections to user guide

--- src/app/user-guide/page.tsx
+++ src/app/user-guide/page.tsx
@@ -1,6 +1,50 @@
 import Navigation from '@/app/navigation'
 import ImageWithDescription from '@/components/user-guide/image-with-description'
 
+const GUIDE_SECTIONS = {
+    USER_ACCOUNTS: {
+        LOGIN_SCREEN: {
+            url: 'https://storage.googleapis.com/ultmt-site-images/login-screen.png',
+            description:
+                "To make your own account, navigate to the 'Account' tab and click 'Create Account'",
+        },
+        CREATE_ACCOUNT: {
+            url: 'https://storage.googleapis.com/ultmt-site-images/create-account.png',
+            description:
+                'Enter in your details. Ensure you have a unique email and username.',
+        },
+        PROFILE_SCREEN: {
+            url: 'https://storage.googleapis.com/ultmt-site-images/profile-screen.png',
+            description:
+                'The main profile screen includes your statistics, recent games, and teams.',
+        },
+        SETTINGS: {
+            url: 'https://storage.googleapis.com/ultmt-site-images/settings.png',
+            description:
+                "Edit settings by clicking 'Settings' in the top right corner of the Profile screen",
+        },
+        REQUESTS: {
+            url: 'https://storage.googleapis.com/ultmt-site-images/manage-requests-from-teams.png',
+            description:
+                "Respond to roster requests from teams by clicking 'Requests' in the top right corner of the Profile screen. You can request teams by clicking the '+' button next to 'Req",
+        },
+    },
+    TEAM_MANAGEMENT: {
+        CREATE_TEAM: {
+            url: 'https://storage.googleapis.com/ultmt-site-images/create-team.png',
+            description:
+                "Create a team by clicking the '+' button next to 'Teams' on the Profile screen. You must include a unique team handle; think if this as a username for your team.",
+        },
+        MANAGE_TEAMS: {
+            url: 'https://storage.googleapis.com/ultmt-site-images/manage-team-screen.png',
+            description:
+                'You can see team members, add managers, request players to join from the Manage Team screen.',
+        },
+    },
+    GAME_PLAY: {},
+    GUESTS: {},
+}
+
 export default function UserGuide() {
     return (
         <div>
@@ -16,19 +60,41 @@ export default function UserGuide() {
                             Creating an Account
                         </span>
                         <ImageWithDescription
-                            url="https://storage.googleapis.com/ultmt-site-images/create-account.png"
-                            description="To make your own account, navigate to the 'Account' tab and click 'Create Account'"
+                            {...GUIDE_SECTIONS.USER_ACCOUNTS.LOGIN_SCREEN}
+                        />
+                        <ImageWithDescription
+                            {...GUIDE_SECTIONS.USER_ACCOUNTS.CREATE_ACCOUNT}
+                        />
+                        <span className="text-xl text-ultmtblue">
+                            Account Actions
+                        </span>
+                        <ImageWithDescription
+                            {...GUIDE_SECTIONS.USER_ACCOUNTS.PROFILE_SCREEN}
+                        />
+                        <ImageWithDescription
+                            {...GUIDE_SECTIONS.USER_ACCOUNTS.SETTINGS}
+                        />
+                        <ImageWithDescription
+                            {...GUIDE_SECTIONS.USER_ACCOUNTS.REQUESTS}
                         />
                     </div>
                     <div>
-                        <span>Managing Teams</span>
+                        <span className="text-2xl text-ultmtyellow">
+                            Managing Teams
+                        </span>
+                        <ImageWithDescription
+                            {...GUIDE_SECTIONS.TEAM_MANAGEMENT.CREATE_TEAM}
+                        />
+                        <ImageWithDescription
+                            {...GUIDE_SECTIONS.TEAM_MANAGEMENT.MANAGE_TEAMS}
+                        />
                     </div>
-                    <div>
+                    {/* <div>
                         <span>Game Play</span>
                     </div>
                     <div>
                         <span>Guests</span>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
--- src/components/user-guide/image-with-description.tsx
+++ src/components/user-guide/image-with-description.tsx
@@ -8,16 +8,16 @@ const ImageWithDescription = ({
     description: string
 }) => {
     return (
-        <>
+        <div className="mb-4">
             <Image
                 alt="Filled in create account screen"
                 src={url}
                 width={160}
                 height={400}
-                className="border border-solid border-ultmtblue rounded self-center"
+                className="border border-solid border-ultmtblue rounded self-center mb-2"
             />
-            <span className="text-gray-300">{description}</span>
-        </>
+            <span className="text-gray-300 text-sm">{description}</span>
+        </div>
     )
 }
 
